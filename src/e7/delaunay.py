14b6e04e26fb852ecbcf7db57b3c6d8a0ea2eba8
Steffen Peikert <steffen.peikert@fau.de>
2024-06-12 14:36:18 +0200

    Fix circumsphere
    BowyerWatson now visualises the step(s).
    
    Moved mesh creation to tetrahedron
    
    Fixed circumsphere calculation

--- src/e7/delaunay.py
+++ src/e7/delaunay.py
@@ -71,6 +71,7 @@ class Tetrahedron:
 
         self._faces = self._calculate_faces()
         self._circumsphere = self._calculate_circumsphere()
+        self._mesh = self._create_mesh()
 
     @property
     def vertices(self):
@@ -84,24 +85,76 @@ class Tetrahedron:
     def faces(self):
         return self._faces
 
+    @property
+    def mesh(self):
+        return self._mesh
+
+    @property
+    def is_super_tetrahedron(self):
+        return np.any(self.indices < 4)
+
     def is_point_in_circumsphere(self, point: np.ndarray):
         return self._circumsphere.is_point_in_sphere(point)
 
+    def _create_mesh(self):
+        edges = []
+
+        for face in self.faces:
+            edges.append((face[0], face[1]))
+            edges.append((face[1], face[2]))
+            edges.append((face[2], face[0]))
+
+        # remove duplicates
+        edges = list(set(tuple(sorted(edge)) for edge in edges))
+
+        lines = [[self.indices.tolist().index(edge[0]), self.indices.tolist().index(edge[1])] for edge in edges]
+
+        lineset = o3d.geometry.LineSet()
+        lineset.points = o3d.utility.Vector3dVector(self.vertices)
+        lineset.lines = o3d.utility.Vector2iVector(lines)
+        if self.is_super_tetrahedron:
+            lineset.colors = o3d.utility.Vector3dVector([[0, 0, 1] for _ in range(len(lines))])
+        else:
+            lineset.colors = o3d.utility.Vector3dVector([[1, 0, 0] for _ in range(len(lines))])
+
+        return lineset
+
     def _ensure_tetrahedron(self):
-        v1 = self.vertices[1] - self.vertices[0]
-        v1 /= np.linalg.norm(v1)
-        v2 = self.vertices[2] - self.vertices[0]
-        v2 /= np.linalg.norm(v2)
-        v3 = self.vertices[3] - self.vertices[0]
-        v3 /= np.linalg.norm(v3)
-
-        normal = np.cross(v1, v2)
-        angle = np.dot(normal, v3)
-
-        assert angle != 0
+        if (
+            np.array_equal(self.vertices[0], self.vertices[1])
+            or np.array_equal(self.vertices[0], self.vertices[2])
+            or np.array_equal(self.vertices[0], self.vertices[3])
+        ):
+            raise ValueError()
 
     def _calculate_circumsphere(self):
+        v0_2 = np.linalg.norm(self.vertices[0]) ** 2
+        v1 = self.vertices[1] - self.vertices[0]
+        v2 = self.vertices[2] - self.vertices[0]
+        v3 = self.vertices[3] - self.vertices[0]
 
+        a = np.array([v1, v2, v3])
+        b = np.array(
+            [
+                [(np.linalg.norm(self.vertices[1]) ** 2 - v0_2) / 2],
+                [(np.linalg.norm(self.vertices[2]) ** 2 - v0_2) / 2],
+                [(np.linalg.norm(self.vertices[3]) ** 2 - v0_2) / 2],
+            ]
+        )
+
+        center = np.linalg.solve(a, b).flatten()
+        radius = np.linalg.norm(self.vertices[0] - center)
+
+        radius_v0 = np.linalg.norm(self.vertices[0] - center)
+        radius_v1 = np.linalg.norm(self.vertices[1] - center)
+        radius_v2 = np.linalg.norm(self.vertices[2] - center)
+        radius_v3 = np.linalg.norm(self.vertices[3] - center)
+
+        assert abs(radius_v0 - radius_v1) < 1 and abs(radius_v0 - radius_v2) < 1 and abs(radius_v0 - radius_v3) < 1
+
+        return Sphere(center, radius)
+
+        # Alternative method
         v1 = self.vertices[1] - self.vertices[0]
         v2 = self.vertices[2] - self.vertices[0]
         v3 = self.vertices[3] - self.vertices[0]
@@ -115,10 +168,10 @@ class Tetrahedron:
         b = np.inner(v2, v2) * np.cross(v3, v1)
         c = np.inner(v3, v3) * np.cross(v1, v2)
 
-        origin = self.vertices[0] + (a + b + c) / (np.linalg.det(m) * 2)
-        radius = np.linalg.norm(origin - v1)
+        center = self.vertices[0] + (a + b + c) / (np.linalg.det(m) * 2)
+        radius = np.linalg.norm(self.vertices[0] - center)
 
-        return Sphere(origin, radius)
+        return Sphere(center, radius)
 
     def _calculate_faces(self):
         return np.array(
@@ -133,16 +186,48 @@ class Tetrahedron:
         )
 
 
-def bowyer_watson(points: np.ndarray):
-    vertices = np.zeros((4 + len(points), 3))
-    vertices[:4] = _create_super_tetrahedron_points(points)
-    super_tetrahedron = Tetrahedron(vertices, np.arange(4))
+class BowyerWatsonVisualiser:
+    def __init__(self, points: np.ndarray) -> None:
+        self._points = points
+        self._point_iter = iter(range(len(self._points)))
 
-    triangulation = [super_tetrahedron]
+        self._vertices = np.zeros((4 + len(points), 3))
+        self._vertices[:4] = self._create_super_tetrahedron_points()
+        self._super_tetrahedron = Tetrahedron(self._vertices, np.arange(4))
 
-    for point_idx, point in enumerate(points):
+        self._triangulation = [self._super_tetrahedron]
+
+        # setup visualization
+        self._renderer = o3d.visualization.VisualizerWithKeyCallback()
+        self._renderer.register_key_callback(32, self._next_iteration)
+        self._renderer.create_window()
+
+        # Adding the mesh once sets the camera bb (which is not updated by draw)
+        self._renderer.add_geometry(self._super_tetrahedron.mesh)
+        self._draw()
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(self._points)
+        self._renderer.add_geometry(pcd)
+
+        self._renderer.run()
+
+    def __del__(self):
+        self._renderer.destroy_window()
+
+    def _next_iteration(self, vis):
+        try:
+            self._iteration(next(iter(self._point_iter)))
+        except StopIteration:
+            # if no more elements are available, clean up
+            self._clean_up()
+
+        self._draw()
+
+    def _iteration(self, point_idx):
+        point = self._points[point_idx]
+
         # Find the tetrahedra which violate the triangulation if the point is inserted
-        invalid_tetrahedra = [tetra for tetra in triangulation if tetra.is_point_in_circumsphere(point)]
+        invalid_tetrahedra = [tetra for tetra in self._triangulation if tetra.is_point_in_circumsphere(point)]
 
         # find the face of the bad tetrahedra - these are the hull of the hole we are going to make
         # We count how often we encounter each face
@@ -156,40 +241,69 @@ def bowyer_watson(points: np.ndarray):
 
         # remove invalid tetrahedra from the triangulation
         for tetra in invalid_tetrahedra:
-            triangulation.remove(tetra)
+            self._triangulation.remove(tetra)
 
         # add the new vertex to our list of vertices
-        vertices[4 + point_idx] = point
+        self._vertices[4 + point_idx] = point
 
         # Triangulate the cavity
         for face in hull_faces:
             indices = np.zeros(4, dtype=int)
             indices[1:4] = face
             indices[0] = point_idx + 4
-            triangulation.append(Tetrahedron(vertices, indices))
+            self._triangulation.append(Tetrahedron(self._vertices, indices))
 
-    # Remove all tetrahedra that are part of the supertetrahedron
-    # I.e., no index belongs to the supertetrahedron vertices
-    final_tetrahedra = [tetra for tetra in triangulation if np.all(tetra.indices > 3)]
+    def _create_super_tetrahedron_points(self):
+        aabb = AABB(self._points)
 
-    return final_tetrahedra
+        vertices = np.array(
+            [
+                np.array([2 * aabb.size, 0, -aabb.size]),
+                np.array([-aabb.size, 2 * aabb.size, -aabb.size]),
+                np.array([-aabb.size, -aabb.size, 2 * aabb.size]),
+                np.array([-aabb.size, -aabb.size, -aabb.size]),
+            ]
+        )
 
+        vertices += aabb.center
 
-def _create_super_tetrahedron_points(points: np.ndarray):
-    aabb = AABB(points)
+        return vertices
 
-    vertices = np.array(
-        [
-            np.array([2 * aabb.size, 0, -aabb.size]),
-            np.array([-aabb.size, 2 * aabb.size, -aabb.size]),
-            np.array([-aabb.size, -aabb.size, 2 * aabb.size]),
-            np.array([-aabb.size, -aabb.size, -aabb.size]),
-        ]
-    )
+    def _clean_up(self):
+        # Remove all tetrahedra that are part of the supertetrahedron
+        # I.e., no index belongs to the supertetrahedron vertices
+        self._triangulation = [tetra for tetra in self._triangulation if not tetra.is_super_tetrahedron]
 
-    vertices += aabb.center
+    def _draw(self):
+        self._renderer.clear_geometries()
 
-    return vertices
+        for tetra in sorted(self._triangulation, key=lambda x: x.is_super_tetrahedron):
+            self._renderer.add_geometry(tetra.mesh, reset_bounding_box=False)
+
+            # sphere = o3d.geometry.TriangleMesh.create_sphere(tetra._circumsphere.radius)
+            # sphere.translate(tetra._circumsphere.center)
+            # sphere.compute_vertex_normals()
+
+            # lines = []
+            # for triangle in sphere.triangles:
+            #     triangle = triangle.tolist()
+            #     lines.extend(
+            #         [
+            #             [triangle[0], triangle[1]],
+            #             [triangle[1], triangle[2]],
+            #             [triangle[2], triangle[0]],
+            #         ]
+            #     )
+
+            # # Create the line set
+            # line_set = o3d.geometry.LineSet(
+            #     points=sphere.vertices,
+            #     lines=o3d.utility.Vector2iVector(lines),
+            # )
+
+            # self._renderer.add_geometry(line_set, reset_bounding_box=False)
+
+        self._renderer.update_renderer()
 
 
 def _create_random_points(xdim, ydim, zdim, n):
@@ -214,56 +328,13 @@ def _create_mesh_points(xdim, ydim, zdim, n):
     return np.vstack([x.ravel(), y.ravel(), z.ravel()]).T
 
 
-def _create_tetrahedron_mesh(vertices):
-    mesh = o3d.geometry.TriangleMesh()
-
-    mesh.vertices = o3d.utility.Vector3dVector(vertices)
-    mesh.triangles = o3d.utility.Vector3iVector(
-        [
-            [0, 1, 2],
-            [1, 3, 2],
-            [3, 0, 2],
-            [0, 1, 3],
-        ]
-    )
-
-    mesh.compute_vertex_normals()
-
-    return mesh
-
-
-def _create_tetrahedron_wireframe(tetra: Tetrahedron):
-
-    edges = []
-    for face in tetra.faces:
-        edges.append((face[0], face[1]))
-        edges.append((face[1], face[2]))
-        edges.append((face[2], face[0]))
-
-    # remove duplicates
-    edges = list(set(tuple(sorted(edge)) for edge in edges))
-
-    lines = [[tetra.indices.tolist().index(edge[0]), tetra.indices.tolist().index(edge[1])] for edge in edges]
-
-    lineset = o3d.geometry.LineSet()
-    lineset.points = o3d.utility.Vector3dVector(tetra.vertices)
-    lineset.lines = o3d.utility.Vector2iVector(lines)
-    lineset.colors = o3d.utility.Vector3dVector([[1, 0, 0] for _ in range(len(lines))])
-
-    return lineset
-
-
-def key_callback(vis):
-    print("Key pressed")
-
-
 def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--num-points",
         "-n",
         type=int,
-        default=4,
+        default=5,
     )
     parser.add_argument(
         "--xdim",
@@ -308,22 +379,9 @@ def _main():
         )
     )
 
-    triangulation = bowyer_watson(points)
+    np.random.seed(0)
 
-    print(f"Found: {len(triangulation)} tetrahedrons!")
-
-    vis = o3d.visualization.VisualizerWithKeyCallback()
-    vis.create_window()
-    # vis.register_key_callback(32, key_callback)
-
-    for tetra in triangulation:
-        # vis.add_geometry(_create_tetrahedron_mesh(tetra.vertices))
-        vis.add_geometry(_create_tetrahedron_wireframe(tetra))
-
-    vis.add_geometry(o3d.geometry.TriangleMesh.create_coordinate_frame())
-
-    vis.run()
-    vis.destroy_window()
+    BowyerWatsonVisualiser(points)
 
 
 if __name__ == "__main__":
